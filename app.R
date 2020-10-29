f6ba9bf41aeb8cda830c4c179af345275073b283
grahammaticalerror <72665297+grahammaticalerror@users.noreply.github.com>
2020-10-28 18:00:55 -0700

    Update app.R

--- app.R
+++ app.R
@@ -4,19 +4,23 @@ library(shinydashboard) # The layout used for the ui page.
 library(leaflet) # Map making. Leaflet is more supported for shiny.
 library(dplyr)  
 
-Allergies <- read.csv2("C:\\Users\\t_kna\\Documents\\R_COVID_APP\\data\\allergies.csv")
-Careplans <- read.csv2("C:\\Users\\t_kna\\Documents\\R_COVID_APP\\data\\careplans.csv")
-Conditions <- read.csv2("C:\\Users\\t_kna\\Documents\\R_COVID_APP\\data\\conditions.csv")
-Devices <- read.csv2("C:\\Users\\t_kna\\Documents\\R_COVID_APP\\data\\devices.csv")
-Encounters <- read.csv2("C:\\Users\\t_kna\\Documents\\R_COVID_APP\\data\\encounters.csv")
-Imaging_Studies <- read.csv2("C:\\Users\\t_kna\\Documents\\R_COVID_APP\\data\\imaging_studies.csv")
-Immunizations <- read.csv2("C:\\Users\\t_kna\\Documents\\R_COVID_APP\\data\\immunizations.csv")
-Medications <- read.csv2("C:\\Users\\t_kna\\Documents\\R_COVID_APP\\data\\medications.csv")
-Observations <- read.csv2("C:\\Users\\t_kna\\Documents\\R_COVID_APP\\data\\observations.csv")
-Organizations <- read.csv2("C:\\Users\\t_kna\\Documents\\R_COVID_APP\\data\\organizations.csv")
-Patients <- read.csv2("C:\\Users\\t_kna\\Documents\\R_COVID_APP\\data\\patients.csv")
-Payers <- read.csv2("C:\\Users\\t_kna\\Documents\\R_COVID_APP\\data\\payers.csv")
-Procedures <- read.csv2("C:\\Users\\t_kna\\Documents\\R_COVID_APP\\data\\procedures.csv")
+#---When running this app on your computer, you will likely have to change the file path. I'm not sure how it's going to work out, honestly, so let me know when you get
+#---a chance to run it on your R console------
+setwd("C:\\Users\\t_kna\\Documents\\R_COVID_APP")
+
+Allergies <- read.csv("C:\\Users\\t_kna\\Documents\\R_COVID_APP\\data\\merged_Allergies.csv")
+Careplans <- read.csv("C:\\Users\\t_kna\\Documents\\R_COVID_APP\\data\\merged_Careplans.csv")
+Conditions <- read.csv("C:\\Users\\t_kna\\Documents\\R_COVID_APP\\data\\merged_Conditions.csv")
+Devices <- read.csv("C:\\Users\\t_kna\\Documents\\R_COVID_APP\\data\\merged_Devices.csv")
+Encounters <- read.csv("C:\\Users\\t_kna\\Documents\\R_COVID_APP\\data\\merged_Encounters.csv")
+Imaging_Studies <- read.csv("C:\\Users\\t_kna\\Documents\\R_COVID_APP\\data\\merged_Imaging_Studies.csv")
+Immunizations <- read.csv("C:\\Users\\t_kna\\Documents\\R_COVID_APP\\data\\merged_Immunizations.csv")
+Medications <- read.csv("C:\\Users\\t_kna\\Documents\\R_COVID_APP\\data\\merged_Medications.csv")
+Observations <- read.csv("C:\\Users\\t_kna\\Documents\\R_COVID_APP\\data\\merged_Observations.csv")
+Organizations <- read.csv("C:\\Users\\t_kna\\Documents\\R_COVID_APP\\data\\merged_Organizations.csv")
+Patients <- read.csv("C:\\Users\\t_kna\\Documents\\R_COVID_APP\\data\\merged_Patients.csv")
+Payers <- read.csv("C:\\Users\\t_kna\\Documents\\R_COVID_APP\\data\\merged_Payers.csv")
+Procedures <- read.csv("C:\\Users\\t_kna\\Documents\\R_COVID_APP\\data\\merged_Procedures.csv")
 
 
 ui <- fluidPage(
